3bbd0629d16640ab11539478b044ddb428f3d6fd
Peter-Jan Gootzen <peterjan@gootzen.net>
2019-06-26 23:29:57 +0200

    Added cli arguments and cleaned up main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,33 @@
+extern crate clap;
+
 mod models;
 mod creation;
 mod gui;
 mod circuit_checking;
 mod tests;
 
+use clap::App;
+use clap::Arg;
+
 use creation::circuit_file_reader::AvansCircuitReader;
 use creation::circuit_file_reader::CircuitReaderTrait;
 use creation::circuit_loader;
-
 use models::circuit::Circuit;
-
-use std::io::stdin;
-use std::io::prelude::*;
-use std::cell::RefCell;
-use crate::models::node::NodeTrait;
+use models::node::NodeTrait;
 
 fn main() {
-    let mut circuit: Option<Circuit> = None;
-    let std_in = stdin();
+    let matches = App::new("circuit_simulator.rs")
+        .version("0.9")
+        .about("")
+        .author("Peter-Jan Gootzen & Bram-Boris Meerlo")
+        .arg(Arg::with_name("circuit_file_name")
+             .help("The circuit that will get loaded")
+             .required(true)
+             .index(1))
+        .get_matches();
 
-    //gui::init(|filepath: String| {
-    //    let circuit_file_reader = AvansCircuitReader{};
-    //    match circuit_file_reader.read_circuit(filepath) {
-    //        Ok(circuit_file) => { 
-    //            circuit = Some(circuit_loader::load_circuit(circuit_file.0, circuit_file.1));
-
-    //            return None;
-    //        },
-    //        Err(_) => Some("Could not load circuit!")
-    //    };
-    //    None
-    //}, || return &circuit);
-    //gtk::main();
-
-    match load_circuit(String::from("./examplecircuit.txt")) {
+    let circuit_file_name = matches.value_of("circuit_file_name").unwrap().to_string();
+    match load_circuit(circuit_file_name) {
         Ok(circuit) => {
             let output_nodess = circuit.get_output_nodes();
             for node in output_nodess.iter() {
@@ -43,29 +37,6 @@ fn main() {
         },
         Err(message) => panic!(message)
     };
-
-//    for line in std_in.lock().lines() {
-//        match line {
-//            Ok(text) => {
-//                if text == String::from("exit") {
-//                    std::process::exit(0);
-//                } else if text == String::from("help") {
-//                    panic!("Created by Peter-Jan and Bram-Boris");
-//                } else if text.starts_with("f ") {
-//                    let filepath = text.split_at(2).1;
-//                    match load_circuit(filepath.to_string()) {
-//                        Ok(circuit) => {
-//                            println!("{}", gui::simulate(&Some(circuit)));
-//                        },
-//                        Err(message) => panic!(message)
-//                    };
-//                }
-//            },
-//            Err(_) => {
-//                println!("ERRR");
-//            }
-//        };
-//    }
 }
  
 fn load_circuit(filepath: String) -> Result<Circuit, &'static str> {
